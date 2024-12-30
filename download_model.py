749a85552ebc57a8abf22c1e9be81c5c54b9480f
Abdessami Guebli <118647191+ab2gbl@users.noreply.github.com>
2024-12-30 13:24:34 +0100

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,8 +1,5 @@
 import kagglehub
     
-# Authenticate
-kagglehub.login() # This will prompt you for your credentials.
-# We also offer other ways to authenticate (credential file & env variables): https://github.com/Kaggle/kagglehub?tab=readme-ov-file#authenticate
 
 path = kagglehub.model_download("abdessamiguebli/ai_detection_model_200k/pyTorch/default")
 
